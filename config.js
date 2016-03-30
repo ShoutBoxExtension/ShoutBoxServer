a5612e5686f953934aa6f23f40243b613daaa316
Arjun Kalburgi <askalburgi@gmail.com>
2016-03-30 16:24:47 -0600

    moving everything to organization

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports={
+	/*
+	* This file contains the configurations information of Twitter login app.
+	* It consists of Twitter app information, database information.
+	*/
+
+	"twitter_api_key": "JeSDrMy0zMDh7Pmt3xWSLy0hh",
+	"twitter_api_secret": "Hfky1qT3IITyANQrA4cOtiUCHujseMV389eKHwLqf7mIuoWRLf",
+	"callback_url": "https://shoutboxextension.herokuapp.com/auth/twitter/callback",
+	// "callback_url": "http://127.0.0.1:3000/auth/twitter/callback",
+	"use_database": "false",
+	"host": "heroku",
+	// "host": "localhost",
+	"username": "root",
+	"password": "",
+	"database": "twitter_login"
+}
